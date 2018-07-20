155364c63413a6670ce55fb726cab59be14f40fe
floydms <floydmsmc@gmail.com>
2018-07-20 11:59:57 +0800

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -15,3 +15,18 @@ return false;
           img.src="https://floydms.github.io/SchoolProjectVWWangara/img/ArtB.jpg";
           return false;
 }
+
+function postcode() {
+    var x, text;
+
+    // Get the value of the input field with id="PCNum" ie the postcode input
+    x = document.getElementById("PCnum").value;
+
+   // If a number between 1000-9999 is entered it is valid
+    if (isNaN(x) || x < 999 || x > 10000) {
+        text = "Invalid Postcode";
+    } else {
+        text = "Information valid for your area will be displayed";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
