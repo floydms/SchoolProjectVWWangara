68b0c42574a0e8c3ce2e6b74d9e44cda5868e89e
floydms <floydmsmc@gmail.com>
2018-07-20 10:58:11 +0800

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,13 @@
 function changeImage()
 {
 var img = document.getElementById("myImage");
-img.src="cs/k.png"
+img.src="https://floydms.github.io/SchoolProjectVWWangara/img/ArtR.jpg"
 return false;
 }
             function changeImage2()
 {
 var img = document.getElementById("myImage");
-img.src="green.png";
+img.src="https://floydms.github.io/SchoolProjectVWWangara/img/ArtBl.jpg";
 return false;
 }
             function changeImage3(){
