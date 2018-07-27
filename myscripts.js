14bbc3469ce5a4ba90b74bc007f2fc4d855e7bc2
floydms <floydmsmc@gmail.com>
2018-07-27 10:33:00 +0800

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,102 @@
+function ArtR()
+ {
+var img = document.getElementById("myImage");
+img.src="https://floydms.github.io/SchoolProjectVWWangara/img/ArtR.jpg"
+return false;
+}
+            function ArtBl()
+{
+var img = document.getElementById("myImage");
+img.src="https://floydms.github.io/SchoolProjectVWWangara/img/ArtBl.jpg";
+return false;
+}
+            function ArtB(){
+         var img = document.getElementById("myImage");
+          img.src="https://floydms.github.io/SchoolProjectVWWangara/img/ArtB.jpg";
+          return false;
+}
+  function ArtW(){
+         var img = document.getElementById("myImage");
+          img.src="https://floydms.github.io/SchoolProjectVWWangara/img/ArtW.jpg";
+          return false;
+}
+function postcode() {
+    var x, text;
+
+    // Get the value of the input field with id="PCNum" ie the postcode input
+    x = document.getElementById("PCnum").value;
+
+   // If a number between 1000-9999 is entered it is valid
+    if (isNaN(x) || x < 999 || x > 10000) {
+        text = "Invalid Postcode";
+    } else {
+        text = "Information valid for your area will be displayed";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
+function GolfR()
+ {
+var img = document.getElementById("myImage");
+img.src="https://floydms.github.io/SchoolProjectVWWangara/img/GolfR.jpg"
+return false;
+}
+            function GolfBl()
+{
+var img = document.getElementById("myImage");
+img.src="https://floydms.github.io/SchoolProjectVWWangara/img/GolfBl.jpg";
+return false;
+}
+            function GolfB(){
+         var img = document.getElementById("myImage");
+          img.src="https://floydms.github.io/SchoolProjectVWWangara/img/GolfB.jpg";
+          return false;
+}
+  function GolfW(){
+         var img = document.getElementById("myImage");
+          img.src="https://floydms.github.io/SchoolProjectVWWangara/img/GolfW.jpg";
+          return false;
+}
+function AmR()
+ {
+var img = document.getElementById("myImage");
+img.src="https://carsales.pxcrush.net/carsales/car/dealer/07t8ennil3guozdeh1080.jpg?aspect=centered&height=552&width=828"
+return false;
+}
+            function AmBl()
+{
+var img = document.getElementById("myImage");
+img.src="https://carimages.com.au/u4dOvDGUqjIWD4-RZjLxn8I6vZ8=/fit-in/800x600/filters:stretch(FFFFFF)/vehicles/redbook/S0000KAH.jpg";
+return false;
+}
+            function AmB(){
+         var img = document.getElementById("myImage");
+          img.src="https://res.cloudinary.com/carsguide/image/private/t_cg_car_l/v1/car/0600/5458/2018_volkswagen_amarok_used_6005458_1.jpg?version=1531838970";
+          return false;
+}
+  function AmW(){
+         var img = document.getElementById("myImage");
+          img.src="https://carsales.pxcrush.net/carsales/car/cil/cc5665781842217699764.jpg?pxc_method=crop&pxc_size=230%2C153";
+          return false;
+}
+function TigR()
+ {
+var img = document.getElementById("myImage");
+img.src="https://floydms.github.io/SchoolProjectVWWangara/img/TigR.jpg"
+return false;
+}
+            function TigBl()
+{
+var img = document.getElementById("myImage");
+img.src="https://floydms.github.io/SchoolProjectVWWangara/img/TigBl.jpg";
+return false;
+}
+            function TigB(){
+         var img = document.getElementById("myImage");
+          img.src="https://floydms.github.io/SchoolProjectVWWangara/img/TigB.jpg";
+          return false;
+}
+  function TigW(){
+         var img = document.getElementById("myImage");
+          img.src="https://floydms.github.io/SchoolProjectVWWangara/img/TigW.jpg";
+          return false;
+}
